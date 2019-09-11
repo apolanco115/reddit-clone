f1dfb4bde41ecb6306e1a96fdb4e9209f058e1c2
Austin Polanco <apolanco115@gmail.com>
2019-09-11 15:24:01 -0400

    cleaned up code; added post creation fuctionality to content-page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
-function postLogin(event) {
+function postData(event) {
    event.preventDefault();
    const email = document.querySelector('.email');
    const password = document.querySelector('.pw');
-   const username = document.querySelector('.username');
+   // const username = document.querySelector('.username');
    console.log(email.value, password.value)
    fetch('http://thesi.generalassemb.ly:8080/login', {
            method: 'POST',
@@ -15,13 +15,17 @@ function postLogin(event) {
            })
    })
    .then((res) => {
-     //console.log(res)
        return res.json();
    })
    .then((res) => {
      console.log(res)
        localStorage.setItem('user', res.token);
-       // createPost();
+       if(res.httpStatus !== 'BAD_REQUEST') {
+        window.location.href = "content-page.html";
+       }else{
+        localStorage.clear();
+       }
+
    })
    .catch((err) => {
        console.log(err);
--- js/posts.js
+++ js/posts.js
@@ -40,3 +40,54 @@ function listSomePosts(event) {
     });
 }
 listSomePosts();
+
+function createPost(event) {
+  event.preventDefault();
+  const title = document.querySelector(".title");
+  const description = document.querySelector(".description");
+  fetch("http://thesi.generalassemb.ly:8080/post", {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("user"),
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      title: title.value,
+      description: description.value
+    })
+  })
+    .then(res => {
+      console.log(res);
+      updateDom(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function updateDom() {
+  document.querySelector(".postForm").style.display = "block";
+  fetch("http://thesi.generalassemb.ly:8080/user/post", {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("user")
+    }
+  })
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      const list = document.querySelector(".posts");
+      const item = document.createElement("li");
+      item.className = "post"
+      const title = document.createElement("h2");
+      const description = document.createElement("p");
+      item.appendChild(title);
+      item.appendChild(description);
+      title.innerText = res[res.length-1].title;
+      description.innerText = res[res.length-1].description;
+      list.appendChild(item);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}

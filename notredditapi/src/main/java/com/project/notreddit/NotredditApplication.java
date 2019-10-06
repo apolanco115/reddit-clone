1876a92d81bfc1d5d806f34d466dda19db2cfd54
Austin Polanco <apolanco115@gmail.com>
2019-10-06 15:38:49 -0400

    added maven dependencies; added packages

---
+++ notredditapi/src/main/java/com/project/notreddit/NotredditApplication.java
@@ -0,0 +1,13 @@
+package com.project.notreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotredditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotredditApplication.class, args);
+	}
+
+}

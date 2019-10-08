7d21debbd59e0a4bc17036df12bd7570500f6a8b
Austin Polanco <apolanco115@gmail.com>
2019-10-08 18:43:27 -0400

    removed superfluous login method in userrespository

--- notredditapi/src/main/java/com/project/notreddit/Repositories/UserRepository.java
+++ notredditapi/src/main/java/com/project/notreddit/Repositories/UserRepository.java
@@ -7,8 +7,4 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     public User findByUsername(String username);
-
-    @Query("FROM User u WHERE u.username = ?1 AND u.password=?2")
-    public User login(String username, String password);
-
 }

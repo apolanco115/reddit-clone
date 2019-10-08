5596939acff68cb113e71a30f074ec30fabb22e3
ggatling <gerda.gatling@gmail.com>
2019-10-08 09:14:57 -0400

    Added User Controller Test and User Service Test

---
+++ notredditapi/src/test/java/com/project/notreddit/controller/UserControllerTest.java
@@ -0,0 +1,70 @@
+package com.project.notreddit.controller;
+
+
+import com.project.notreddit.Config.JwtUtil;
+import com.project.notreddit.Controllers.UserController;
+import com.project.notreddit.Services.UserService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloWorld_ReturnString_Success() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/hello")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+    }
+    @Test
+    public void login_Success() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("mickey","mouse"));
+
+        when(userService.login(any())).thenReturn("123456");
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"token\":\"123456\"}"))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    private static String createUserInJson (String name, String password) {
+        return "{ \"name\": \"" + name + "\", " +
+                "\"password\":\"" + password + "\"}";
+    }
+}
+

f27e91871331e75135d55f7eef89fc608d610d25
Austin Polanco <apolanco115@gmail.com>
2019-10-09 08:32:44 -0400

    moved buisness logic for coomment deletion from controller to service

--- notredditapi/src/main/java/com/project/notreddit/Services/CommentServiceImpl.java
+++ notredditapi/src/main/java/com/project/notreddit/Services/CommentServiceImpl.java
@@ -6,6 +6,8 @@ import com.project.notreddit.Models.User;
 import com.project.notreddit.Repositories.CommentRepository;
 import com.project.notreddit.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -43,10 +45,19 @@ public class CommentServiceImpl implements CommentService{
 
     @Override
     @Transactional
-    public void deleteCommentById(Long commentId){
+    public ResponseEntity deleteCommentById(Long commentId){
         Comment comment = commentRepository.findById(commentId).get();
+        Authentication auth = authImpl.getAuthentication();
+        Long currUserId = userService.getUser(auth.getName()).getId();
+        Long commentUserId = comment.getUser().getId();
         User user = userRepository.findById(comment.getUser().getId()).get();
-        user.getComments().remove(comment);
+
+        if(currUserId == commentUserId) {
+            user.getComments().remove(comment);
+        }else {
+            return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
